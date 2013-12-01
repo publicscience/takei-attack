377c24a71a35553b3f614656a80fca5a98c173f2
Francis Tseng <ftzeng@gmail.com>
2013-12-01 15:04:34 -0500

    Initial Commit

---
+++ brendan game/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  brendan game
+//
+//  Created by Francis Tseng on 12/1/13.
+//  Copyright Francis Tseng 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ brendan game/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  brendan game
+//
+//  Created by Francis Tseng on 12/1/13.
+//  Copyright Francis Tseng 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
